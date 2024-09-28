a788c620960a0aec7c9feaf55dd67f3d842dfbd3
Priyanshu85277 <166702264+Priyanshu85277@users.noreply.github.com>
2024-09-28 19:56:54 +0530

    Add files via upload

---
+++ yolo.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO 
+model = YOLO('best.pt')  # load a pretrained model (recommended for training)
+
+
+# Run inference with the YOLOv8n model on the 'bus.jpg' image
+results = model.predict(source='test/WhatsApp Image 2024-09-28 at 18.19.47_4d1462e1.jpg',conf=0.25,save=True)
+print(results)
