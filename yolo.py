9350f348c62ee64f16bec0188603ea0bff020cf1
Priyanshu85277 <166702264+Priyanshu85277@users.noreply.github.com>
2024-09-28 19:59:15 +0530

    yolo.py

--- yolo.py
+++ yolo.py
@@ -3,5 +3,5 @@ model = YOLO('best.pt')  # load a pretrained model (recommended for training)
 
 
 # Run inference with the YOLOv8n model on the 'bus.jpg' image
-results = model.predict(source='test/WhatsApp Image 2024-09-28 at 18.19.47_4d1462e1.jpg',conf=0.25,save=True)
+results = model.predict(source='WhatsApp Image 2024-09-28 at 18.19.47_4d1462e1.jpg',conf=0.25,save=True)
 print(results)
